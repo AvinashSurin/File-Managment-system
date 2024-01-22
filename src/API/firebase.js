f2f6f1ce503f3e28ff2207c1f5627663ff585112
Avinash Surin <88780535+AvinashSurin@users.noreply.github.com>
2024-01-22 22:26:53 +0530

    Update firebase.js

--- src/API/firebase.js
+++ src/API/firebase.js
@@ -4,12 +4,12 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCt_OjTCnKYUWK3jPzgerUXEx9ZgW8B6sQ",
-  authDomain: "react-file-manager-c55fb.firebaseapp.com",
-  projectId: "react-file-manager-c55fb",
-  storageBucket: "react-file-manager-c55fb.appspot.com",
-  messagingSenderId: "842331486811",
-  appId: "1:842331486811:web:8b3fff7615f274da1f9d7e"
+  apiKey: process.env.AIzaSyCt_OjTCnKYUWK3jPzgerUXEx9ZgW8B6sQ,
+  authDomain: process.env.React_App_authDomain,
+  projectId: process.env.React_App_projectId,
+  storageBucket: process.env.React_App_storageBucket,
+  messagingSenderId: process.env.React_App_messagingSenderId,
+  appId: process.env.React_App_appId,
 };
 
 firebase.initializeApp(firebaseConfig);
